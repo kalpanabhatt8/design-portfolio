95287c7c2f9d7159ab165e7154b3113d2f0d2cfd
kalpanabhatt8 <kalpanabhatt818@gmail.com>
2025-04-13 10:10:13 +0530

    --layout page

---
+++ src/Layout.jsx
@@ -0,0 +1,60 @@
+import { Outlet, useLocation } from 'react-router-dom'
+import { useEffect } from "react"
+
+const Layout = () => {
+    const { pathname } = useLocation()
+    useEffect(() => {
+        const metaTags= {
+            '/': {
+                title: "Home",
+                description: "This is the home page of the website",
+                keywords: 'home, react, website',
+            },
+            '/aboutus': {
+                title: 'About Us',
+                description: 'Learn more about us.',
+                keywords: 'about us, company, information',
+            },
+            '/services': {
+                title: 'Our Services',
+                description: 'Explore the services we offer.',
+                keywords: 'services, offerings, business',
+            },
+            '/blog': {
+                title: 'Blog',
+                description: 'Read our latest blog posts.',
+                keywords: 'blog, articles, news',
+            },
+            '/contact-us': {
+                title: 'Contact Us',
+                description: 'Get in touch with us.',
+                keywords: 'contact, reach out, support',
+            },
+        }
+        const currentMeta = metaTags[pathname] || {
+            title: 'Empower Edu',
+            description: 'This is default information '
+        }
+
+        document.title = currentMeta.title
+        let descriptionTag = document.querySelector('meta[name="description"]');
+        if (!descriptionTag) {
+            descriptionTag = document.createElement('meta');
+            descriptionTag.setAttribute('name', 'description');
+            document.head.appendChild(descriptionTag);
+        }
+        descriptionTag.setAttribute('content', currentMeta.description)
+    })
+
+
+    return (
+        <>
+            {/* <Header /> */}
+            <p>This is layout</p>
+            <Outlet />
+            {/* <Footer /> */}
+        </>
+    )
+}
+
+export default Layout
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { useEffect } from 'react'
+import  ReactDOM  from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Layout from './Layout';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+const AppLayout= () => (
+  <>
+    <ScrollToTop />
+    <Layout />
+  </>
+);
+
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element: <AppLayout />,
+    // children:[
+    //   {
+    //     path:'/',
+    //     element:<Home/>
+    //   },
+    
+    // ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
