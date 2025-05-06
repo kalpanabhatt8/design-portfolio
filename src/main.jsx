6fbdb0827b38d280b9d084823d8a176eaf7ab95f
Kalpana Bhatt <kalpanabhatt818@gmail.com>
2025-05-06 12:23:42 +0530

    --lock design

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,56 @@
-import React, { useEffect } from 'react'
-import  ReactDOM  from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom'
-import './index.css'
-import Layout from './Layout';
+// import React, { useEffect } from 'react'
+// import  ReactDOM  from 'react-dom/client'
+// import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom'
+// import './index.css'
+// import Layout from './Layout';
 
 
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
+// const ScrollToTop = () => {
+//   const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
 
-  return null;
-};
+//   return null;
+// };
 
-const AppLayout= () => (
-  <>
-    <ScrollToTop />
-    <Layout />
-  </>
-);
+// const AppLayout= () => (
+//   <>
+//     <ScrollToTop />
+//     {/* <Layout /> */}
+//   </>
+// );
 
 
-const router=createBrowserRouter([
-  {
-    path:'/',
-    element: <AppLayout />,
-    // children:[
-    //   {
-    //     path:'/',
-    //     element:<Home/>
-    //   },
+// const router=createBrowserRouter([
+//   {
+//     path:'/',
+//     element: <AppLayout />,
+//     // children:[
+//     //   {
+//     //     path:'/',
+//     //     element:<Home/>
+//     //   },
     
-    // ]
-  }
-])
+//     // ]
+//   }
+// ])
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+
+//   <React.StrictMode>
+//     <RouterProvider router={router}/>
+//   </React.StrictMode>,
+// )
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
