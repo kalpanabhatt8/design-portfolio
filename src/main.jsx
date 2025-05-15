a31cf092eb8e487927fd1cc4680c04329dba7504
kalpanabhatt8 <kalpanabhatt818@gmail.com>
2025-05-15 19:19:47 +0530

    -- main file added

--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,52 @@
-// import React, { useEffect } from 'react'
-// import  ReactDOM  from 'react-dom/client'
-// import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom'
-// import './index.css'
-// import Layout from './Layout';
-
-
-// const ScrollToTop = () => {
-//   const { pathname } = useLocation();
-
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//   }, [pathname]);
-
-//   return null;
-// };
-
-// const AppLayout= () => (
-//   <>
-//     <ScrollToTop />
-//     {/* <Layout /> */}
-//   </>
-// );
-
-
-// const router=createBrowserRouter([
-//   {
-//     path:'/',
-//     element: <AppLayout />,
-//     // children:[
-//     //   {
-//     //     path:'/',
-//     //     element:<Home/>
-//     //   },
-    
-//     // ]
-//   }
-// ])
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-
-//   <React.StrictMode>
-//     <RouterProvider router={router}/>
-//   </React.StrictMode>,
-// )
-// main.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider, createBrowserRouter, useLocation } from 'react-router-dom';
 import './index.css';
+import Layout from './Layout';
+import Home from './components/Home';
+
+
+
+// ScrollToTop component defined inline
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+// Layout component that includes ScrollToTop
+const AppLayout= () => (
+  <>
+    <ScrollToTop />
+    <Layout />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />, // Use AppLayout which includes ScrollToTop
+    children: [
+      { path: '/', element: <Home /> },
+      // { path: '/aboutus', element: <About /> },
+      // { path: '/services', element: <Services /> },
+      // { path: '/blog', element: <Blog /> },
+      // { path: '/blog/:id', element: <BlogPage /> },
+      // { path: '/insights', element: <Insights /> },
+      // { path: '/gallery', element: <Gallery /> },
+      // { path: '/contact-us', element: <Contact /> },
+      // { path: '/login', element: <LoginAndSignup /> },
+      // { path: '/signup', element: <LoginAndSignup /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
